d32ce055e7677e13d59b7503d37e12364632abef
Pramod <pramod4342@gmail.com>
2025-04-30 12:16:36 +0530

    /

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import TotalScore from "./totalScore";
+import NumberSelector from "./NumberSelector";
+import styled from "styled-components";
 const GamePlay = () => {
   return (
-    <main>
+
+    <MainContainer>
+      <div className="topSection">
       <TotalScore />
-    </main>
+      <NumberSelector/>
+      </div>
+    </MainContainer>
   );
 };
 export default GamePlay;
+
+const MainContainer = styled.main`
+.topSection{
+  display : flex;
+  justify-content : space-between;
+  align-items : center;
+}
+   
+`
